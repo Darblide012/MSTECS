c7bb11d123a5aab801b7dbebb35374ae95050ed7
Darblide012 <68399627+Darblide012@users.noreply.github.com>
2021-11-16 16:12:18 -0600

    Implementacion de data

---
+++ MSTECS/Data/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MSTECS.Data
+{
+    class Database
+    {
+
+        
+
+        [Table  "Colummnna"]
+        public ColumnAttribute Columnas { get; set; }
+
+        [Column]
+        private ColumnAttribute Nombre { get; set; }
+
+        [Column]
+
+
+    }
+}
---
+++ MSTECS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MSTECS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+}
